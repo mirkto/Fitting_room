b3f21aff99c83ddc2a37955ed33bbf8e08926801
mirkto <thehisev@mail.ru>
2021-08-22 20:25:53 +0300

    add sing_page first version, not functional

--- app/src/main/java/com/sberclass/accelerator/startup/fitting_room/MainActivity.java
+++ app/src/main/java/com/sberclass/accelerator/startup/fitting_room/MainActivity.java
@@ -2,15 +2,32 @@ package com.sberclass.accelerator.startup.fitting_room;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button  btn_start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        addListenerOnButton();
+    }
+
+    public void addListenerOnButton() {
+        btn_start = (Button) findViewById(R.id.btn_start);
+        btn_start.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(".MainSing");
+                        startActivity(intent);
+                    }
+                }
+        );
     }
 }
---
+++ app/src/main/java/com/sberclass/accelerator/startup/fitting_room/MainSing.java
@@ -0,0 +1,13 @@
+package com.sberclass.accelerator.startup.fitting_room;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainSing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sing);
+    }
+}
